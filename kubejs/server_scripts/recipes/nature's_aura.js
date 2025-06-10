ffdb8b48e6837e8e6bcabb7ed7c6680f165ec7b2
BarMaxx <98416932+BarMaxx@users.noreply.github.com>
2025-06-10 15:29:01 +0300

    Changed recipes

--- kubejs/server_scripts/recipes/nature's_aura.js
+++ kubejs/server_scripts/recipes/nature's_aura.js
@@ -4,7 +4,7 @@ ServerEvents.recipes(event => {
 	event.shapeless('naturesaura:aura_bottle[naturesaura:aura_bottle_data={aura_type:"naturesaura:overworld"}]', [
 		'naturesaura:aura_bottle[naturesaura:aura_bottle_data={aura_type:"naturesaura:other"}]',
 		'aether:ambrosium_shard'
-	]))
+	])
 	
 	
 	event.remove({output: 'naturesaura:bottle_two_the_rebottling'})
---
+++ kubejs/server_scripts/recipes/primal_magick.js
@@ -0,0 +1,20 @@
+ServerEvents.recipes(event => {
+	
+	
+	event.remove({output: 'primalmagick:mundane_wand'})
+	event.shaped(
+	  Item.of('primalmagick:mundane_wand'), 
+	  [
+	    ' BC',
+		' AB',
+		'A  '
+	  ], 
+	  {
+		A: '#c:rods/wooden',
+		B: '#primalmagick:essences/terrestrial_dusts',
+		C: 'twilightforest:naga_scale'
+	  }
+	)
+	
+	
+})
