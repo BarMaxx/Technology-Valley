e7224125edc7ef37119365a57bfc1f2a2b45a2ce
BarMaxx <98416932+BarMaxx@users.noreply.github.com>
2025-06-10 13:12:34 +0300

    Changed recipes

--- kubejs/server_scripts/recipes/nature's_aura.js
+++ kubejs/server_scripts/recipes/nature's_aura.js
@@ -4,25 +4,7 @@ ServerEvents.recipes(event => {
 	event.shapeless('naturesaura:aura_bottle[naturesaura:aura_bottle_data={aura_type:"naturesaura:overworld"}]', [
 		'naturesaura:aura_bottle[naturesaura:aura_bottle_data={aura_type:"naturesaura:other"}]',
 		'aether:ambrosium_shard'
-	])
-	
-	
-	event.remove({output: 'naturesaura:offering_table'})
-	event.shaped(
-	  Item.of('naturesaura:offering_table'), 
-	  [
-	    'ABA',
-		'FAD',
-		'CCC'
-	  ], 
-	  {
-		A: 'naturesaura:infused_stone',
-		B: 'naturesaura:infused_iron',
-		C: '#minecraft:logs',
-		D: 'naturesaura:token_sorrow',
-		F: 'naturesaura:token_fear'
-	  }
-	)
+	]))
 	
 	
 	event.remove({output: 'naturesaura:bottle_two_the_rebottling'})
@@ -52,6 +34,24 @@ ServerEvents.recipes(event => {
 		A: 'minecraft:short_grass',
 		B: 'aether:ambrosium_shard',
 		C: '#minecraft:leaves'
+	  }
+	)
+	
+	
+	event.remove({output: 'naturesaura:offering_table'})
+	event.shaped(
+	  Item.of('naturesaura:offering_table'), 
+	  [
+	    'BCB',
+		'DBE',
+		'AAA'
+	  ], 
+	  {
+		A: '#minecraft:logs',
+		B: 'naturesaura:infused_stone',
+		C: 'silentgear:crimson_steel_ingot',
+		D: 'naturesaura:token_fear',
+		E: 'naturesaura:token_sorrow'
 	  }
 	)
 	
